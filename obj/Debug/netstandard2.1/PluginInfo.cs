6759f95253cc5435e38952aaeb0111e308134001
Cyber <38331378+LeetMore@users.noreply.github.com>
2023-05-01 23:40:42 +0500

    vs

---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RPGMods
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "RPGMods";
+        public const string PLUGIN_NAME = "RPGMods";
+        public const string PLUGIN_VERSION = "1.2.4";
+    }
+}
